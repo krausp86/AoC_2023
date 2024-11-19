c4a13de9c9328547d31635f643d4371cea794e91
Patrick Kraus-Füreder <patrick.kraus-fuereder@kmlvision.com>
2024-11-19 07:02:31 +0100

    Basic Structure of Day 3 Part 1 included

---
+++ 03/part_1.py
@@ -0,0 +1,22 @@
+# Advent of Code 2023 - Day 3 - Part 1
+# Patrick Kraus-Füreder
+# 19.11.2024
+
+import pytest
+import re
+import numpy as np
+
+def run_puzzle(test=False):
+    return 0
+
+def is_adjacent_to_symbol(number, matrix):
+    return 0
+
+def test_is_adjacent_to_symbol():
+    assert True
+
+def test_run_puzzle():
+    assert True
+
+if __name__ == '__main__':
+    print(run_puzzle())
